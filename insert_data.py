1651c3e40219d9dead6d628588a77703babee58b
flourenco-daniel <danielferreiralourenco95@gmail.com>
2025-08-06 15:48:33 -0300

    inserindo multiplos documentos

--- insert_data.py
+++ insert_data.py
@@ -7,17 +7,26 @@ def inserindo_document():
         db = client.aula
         collection = db.pedidos
 
-        documento = {
-            "cliente": "Nei",
-            "produto": "Produto_1",
+        documento = [
+            {
+            "cliente": "Rui",
+            "produto": "Produto_2",
             "autor": "Curso NOSQL",
-            "qtd": "8",
-            "peso": "1",
+            "qtd": "9",
+            "peso": "10",
             "unidade_medida": "kg"
-            }
+            },
+            {
+            "cliente": "Lia",
+            "produto": "Produto_3",
+            "autor": "Curso NOSQL",
+            "qtd": "15",
+            "peso": "200",
+            "unidade_medida": "kg"
+            }]
 
-        resultado_insercao = collection.insert_one(documento)
-        print(f"Documento inserido com ID: {resultado_insercao.inserted_id}")
+        resultado_insercao = collection.insert_many(documento) #uso o insert_one se eu quiser somente 1 doc e o insert_many se quiser inserir mais de um
+        print(f"Documento inserido com ID: {resultado_insercao.inserted_ids}") #da mesma forma, inserted_id para um e ids para vários
 
     except Exception as e:
         print(f"Erro: {e}")
