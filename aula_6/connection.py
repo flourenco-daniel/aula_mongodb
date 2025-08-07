2dce7b564a5e60f1dccb9e4a9da9bdec6241efe5
flourenco-daniel <danielferreiralourenco95@gmail.com>
2025-08-07 10:42:26 -0300

    connection

---
+++ aula_6/connection.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+def connection(host, port, database, colecao, documento):
+
+    try:
+        host = host
+        port = port
+        client = MongoClient(f'mongodb://{host}:{port}/')
+        client.admin.command('ismaster')
+        print(f"Conexão com ")
+        db = client.database
+        collection = db.colecao
+
+        documento = documento
+        resultado_insercao = collection.insert_many(documento)
+        print(f"Documento inserido com ID: {resultado_insercao.inserted_ids}")
+
+    except Exception as e:
+        print(f"Erro:{e}")
+        return None
+    finally:
+        client.close()
+
+if __name__ == "__main__":
+    connection()
